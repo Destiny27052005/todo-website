aef163140f45e9b0cf340f13ce8f6428f033c743
Adekunle Oluwatobiloba <146554573+Destiny27052005@users.noreply.github.com>
2026-04-15 00:01:54 +0100

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import ProfileCard from "./components/ProfileCard"
 function App() {
 
   return (
-    <>
-      <h1>Profile</h1>
+    <div className="flex flex-col min-h-screen justify-center place-items-center">
+      <h1>Profile Card</h1>
       <ProfileCard/>
-    </>
+    </div>
   )
 }
 
